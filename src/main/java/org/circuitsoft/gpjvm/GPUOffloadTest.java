d226111f36152248111d6eb4ebe172b438415b62
mastercoms <circuitsoft@tuta.io>
2017-04-12 00:42:43 -0400

    Initial commit

---
+++ src/main/java/org/circuitsoft/gpjvm/GPUOffloadTest.java
@@ -0,0 +1,32 @@
+package org.circuitsoft.gpjvm;
+
+import java.util.stream.IntStream;
+
+public class GPUOffloadTest {
+    public static void main(String[] args) {
+        long initStart = System.currentTimeMillis();
+        int length = 100000;
+        int[] a = new int[length];
+        int[] b = new int[length];
+        int[] result = new int[length];
+
+        IntStream.range(0, length).parallel().forEach(p -> {
+            a[p] = 1;
+            b[p] = 2;
+        });
+        long initEnd = System.currentTimeMillis();
+
+        long runStart = System.currentTimeMillis();
+        IntStream.range(0, length).parallel().forEach(p -> result[p] = a[p] + b[p]);
+        long runEnd = System.currentTimeMillis();
+
+        long termStart = System.currentTimeMillis();
+        IntStream.range(0, length).forEach(p -> System.out.println(result[p] + ", " + a[p] + ", " + b[p]));
+        long termEnd = System.currentTimeMillis();
+
+        System.out.println("Init time (ms): " + (initEnd - initStart));
+        System.out.println("Main run time (ms): " + (runStart - runEnd));
+        System.out.println("Termination time (ms): " + (termEnd - termStart));
+        System.out.println("Total time (ms): " + (termEnd - initStart));
+    }
+}
